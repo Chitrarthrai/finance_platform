8fc1ce6768045d43efe2eaecc4d99e8422b8afc0
Chitrarth <chitrarthrai10@gmail.com>
2024-07-21 18:24:58 +0530

    convert column.ts to column.tsx to  use js inside we have added checkbox  and button added comdef to sort using data-table and react-table added filter added feature to show  what we are filtering page.tsx ,in data-table has the function of filtering, sorting, paging and row selection

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Plus } from "lucide-react";
 
@@ -6,47 +6,48 @@ import { useNewAccounts } from "@/features/accounts/hooks/use-new-account";
 
 import { Button } from "@/components/ui/button";
 import { DataTable } from "@/components/data-table";
-import {
-    Card,
-    CardContent,
-    CardHeader,
-    CardTitle
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 import { columns, Payment } from "./columns";
 
-
 const data: Payment[] = [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    }
-]
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "728ed52f",
+    amount: 500,
+    status: "success",
+    email: "a@example.com",
+  },
+];
 
 const AccountsPage = () => {
+  const newAccount = useNewAccounts();
 
-    const newAccount =  useNewAccounts();
-
-    return ( 
-        <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
-            <Card className="border-none drop-shadow-sm">
-                <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
-                    <CardTitle className="text-xl line-clamp-1">
-                        Accounts Page
-                    </CardTitle>
-                    <Button onClick={newAccount.onOpen} size="sm">
-                        <Plus className="size-4 mr-2"/>
-                        Add new
-                    </Button>
-                </CardHeader>
-                <CardHeader>
-                    <DataTable columns={columns} data={data} />
-                </CardHeader>
-            </Card>
-        </div>
-    );
+  return (
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="text-xl line-clamp-1">Accounts Page</CardTitle>
+          <Button onClick={newAccount.onOpen} size="sm">
+            <Plus className="size-4 mr-2" />
+            Add new
+          </Button>
+        </CardHeader>
+        <CardHeader>
+          <DataTable 
+            columns={columns} 
+            data={data} 
+            filterKey="email"
+          />
+        </CardHeader>
+      </Card>
+    </div>
+  );
 };
- 
+
 export default AccountsPage;
