bf3b6f703b2d9e611e3813c77d56edea9262b09b
Chitrarth <chitrarthrai10@gmail.com>
2024-07-25 01:43:57 +0530

    added PATCH route in route.ts in account.ts  we have use .Patch to edit and added use-edit-accounts.ts to get the account we need to edit and in edit-account-sheet added useEditAccount api

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -13,8 +13,9 @@ const app = new Hono().basePath('/api');
 const routes = app
     .route("/accounts", accounts);
 
-export const GET = handle(app)
-export const POST = handle(app)
+export const GET = handle(app);
+export const POST = handle(app);
+export const PATCH = handle(app);
 
 export type AppType = typeof routes;
 
--- features/accounts/components/edit-account-sheet.tsx
+++ features/accounts/components/edit-account-sheet.tsx
@@ -4,7 +4,7 @@ import { Loader2 } from "lucide-react";
 import { useGetAccount } from "@/features/accounts/api/use-get-account";
 import { useOpenAccount } from "@/features/accounts/hooks/use-open-account";
 import { AccountForm } from "@/features/accounts/components/account-form";
-import { useCreateAccount } from "@/features/accounts/api/use-create-account";
+import { useEditAccount } from "@/features/accounts/api/use-edit-account";
 
 import { insertAccountSchema } from "@/db/schema";
 import {
@@ -21,17 +21,19 @@ const formSchema = insertAccountSchema.pick({
 
 type FormValues = z.input<typeof formSchema>;
 
-export const EditAccountSheet = () =>{
+export const  EditAccountSheet = () =>{
     const {isOpen, onClose, id } = useOpenAccount();
 
-    const mutation = useCreateAccount();
+    const editMutation = useEditAccount(id);
+    const accountQuery = useGetAccount(id);
 
-    const accountQuery = useGetAccount(id)
+    const isPending = 
+        editMutation.isPending
 
     const isLoading = accountQuery.isLoading;
 
     const onSubmit = (values: FormValues)=>{
-        mutation.mutate(values, {
+        editMutation.mutate(values, {
             onSuccess: () => {
                 onClose();
             },
@@ -65,7 +67,7 @@ export const EditAccountSheet = () =>{
                         <AccountForm 
                             id={id}
                             onSubmit={onSubmit} 
-                            disabled={mutation.isPending}
+                            disabled={isPending}
                             defaultValues={defaultValues}
                         />
                     )
