4146d65142fe73e3eeac6ae44bd8d9fb62144b20
Chitrarth <chitrarthrai10@gmail.com>
2024-07-25 00:40:18 +0530

    in sheet-provider we have added EditAccountSheet this sheet is responsible for edit values using id we are using use-get-accounts.ts to get the specific account added dropdown from shadcn  which is used in actions.tsx for dorpdown menu for edit added in columns.tsx to use edit option in column  and in account.ts we have added a get method to get the account to edit similar to earlier get account

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -2,7 +2,9 @@
 
 import { useMountedState } from "react-use";
 
-import { NewAccountSheet } from "@/features/accounts/components/new-accounts-sheet"
+import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
+
+import { EditAccountSheet } from "@/features/accounts/components/edit-account-sheet";
 
 export const SheetProvider = () =>{
 
@@ -13,6 +15,7 @@ export const SheetProvider = () =>{
     return(
         <>
             <NewAccountSheet />
+            <EditAccountSheet />
         </>
     );
 };
