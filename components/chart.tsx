c387f62efadd7c8ce4e89f8d3c99ddd549a70fe1
Chitrarth <chitrarthrai10@gmail.com>
2024-08-18 12:17:31 +0530

    we have chart.tsx component using shadcn which will display the data in chart then we have developed area-variant.tsx variant that display the data in graph with custom color for income and expenses with the hep of recharts and custom-tooltip.tsx is using separator from shadcn and and format date and currency to display if we hover over the graph

--- components/chart.tsx
+++ components/chart.tsx
@@ -1,3 +1,14 @@
+import { FileSearch } from "lucide-react";
+
+import {
+    Card,
+    CardHeader,
+    CardTitle,
+    CardContent
+} from "@/components/ui/card"
+
+import { AreaVariant } from "@/components/area-variant";
+
 type Props = {
     data?: {
         date: string;
@@ -8,8 +19,25 @@ type Props = {
 
 export const Chart = ({ data =  [] }: Props) => {
     return (
-        <div>
-            Chart
-        </div>
+        <Card className="border-none drop-shadow-sm">
+            <CardHeader className="flex space-y-2 lg:space-y-0 lg:flex-row lg:items-center justify-between">
+                <CardTitle className="text-xl line-clamp-1">
+                    Transactions
+                </CardTitle>
+                {/* ToDo: Add Select */}
+            </CardHeader>
+            <CardContent>
+                {data.length === 0 ? (
+                    <div className="flex flex-col gap-y-4 items-center justify-center h-[350px] w-full">
+                        <FileSearch  className="size-6 text-muted-foreground"/>
+                        <p className="text-muted-foreground text-sm">
+                            No Data for this Period
+                        </p>
+                    </div>
+                ) : (
+                    <AreaVariant data={data}/>
+                )}
+            </CardContent>
+        </Card>
     );
 };
